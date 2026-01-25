70f6dad0a9e43e37444baf0335b3f22bbbf7bb2f
theothermatt76 <34555031+theothermatt76@users.noreply.github.com>
2026-01-24 20:03:57 -0800

    Update setup.sh
    some comments to explain my thinking

--- setup.sh
+++ setup.sh
@@ -7,10 +7,14 @@ else
     echo "Setting up environment for $1"
 DOMAIN=$1
 mkdir $DOMAIN
+#Somewhere to put your recon output.
 mkdir $DOMAIN/recon
+#Organize your notes.
 mkdir $DOMAIN/notes
 echo "##This is a blank scratch pad. Use it for notes to yourself, weird finds you want to look at later, credentials discovered, etc.##" > $DOMAIN/notes/scratchpad.txt
+#A place for screenshots, files, binaries...
 mkdir $DOMAIN/artifacts
+#Put it all together and keep it here.
 mkdir $DOMAIN/report
 echo "Setup complete! You can now access your environment at ./$DOMAIN"
 echo "Happy hacking!"
