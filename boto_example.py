9fb2414eb452aa5e01ff43757bcfc26515ef4be2
theothermatt76 <34555031+theothermatt76@users.noreply.github.com>
2026-02-06 07:11:36 -0800

    some things I want to keep
    I dont do this often enough

---
+++ boto_example.py
@@ -0,0 +1,7 @@
+import boto3 #always.
+
+#create a client. this can be any service not just ec2...
+ec2_client = boto3.client("ec2")
+
+#more? https://github.com/awsdocs/aws-sdk-examples/tree/main/python/example_code or https://docs.aws.amazon.com/code-library/latest/code_example_library_by_service.html
+
